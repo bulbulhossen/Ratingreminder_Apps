7eccb28ec768b04250212f1ec0b3b97b829807a0
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-29 10:02:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appsd/ratingreminderapps/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.appsd.ratingreminderapps;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.equasoft.ratingreminder.RatingReminder;
+import net.equasoft.ratingreminder.type.AlgoType;
+import net.equasoft.ratingreminder.type.RatingDialogType;
+import net.equasoft.ratingreminder.type.StoreType;
+
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*More details : https://github.com/openkwaky/ratingreminder*/
+
+/*
+        RatingReminder reminder = new RatingReminder(this);
+        reminder.setAppName(getString(R.string.app_name));
+        reminder.setAlgoType(AlgoType.REGULAR_ALGO);
+        reminder.setDialogType(RatingDialogType.IMAGE_DIALOG);
+        reminder.setGap(4);
+
+        ArrayList<StoreType> storeTypes = new ArrayList<StoreType>();
+        storeTypes.add(StoreType.GOOGLE_PLAYSTORE);
+        storeTypes.add(StoreType.AMAZON_STORE);
+        storeTypes.add(StoreType.KURIO_STORE);
+        storeTypes.add(StoreType.YANDEX_STORE);
+        reminder.setStoreTypes(storeTypes);
+
+        reminder.process();
+*/
+
+
+        // Initialize RatingReminder object
+
+        RatingReminder reminder = new RatingReminder(this);
+
+        // Give a name that will be used in dialogs for your app
+        reminder.setAppName(getString(R.string.app_name));
+
+        //Select an algo
+        reminder.setAlgoType(AlgoType.NO_GAP_ALGO); // NO_GAP_ALOG triggers every time
+
+
+
+        // Select a dialog type
+        reminder.setDialogType(RatingDialogType.BASIC_DIALOG); // basic dialog uses default system display
+
+
+        // Configure a store
+        ArrayList<StoreType> storeTypes = new ArrayList<>();
+        storeTypes.add(StoreType.GOOGLE_PLAYSTORE);
+        reminder.setStoreTypes(storeTypes);
+
+
+        // Trigger the dialog if algo decides to
+        reminder.process();
+
+
+    }
+}
